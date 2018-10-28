615415dc7fe81f4ee8b4b14a217552cfdf237a82
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-10-28 18:45:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World"
+  end
+
+  get '/name' do
+    "My name is Nathan Gezahegne"
+  end
+
+  get '/hometown' do
+    "My hometown is Addis"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Medagna Neh Geta"
+  end
 end
