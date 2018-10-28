1474c56866257e6f54dcbe8ce55409018b754604
Ana Harris <vana1990@gmail.com>
2018-10-28 18:28:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Ana."
+  end
+
+  get '/hometown' do
+    "My hometown is Nis, Serbia."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Cheerish the day' by Sade."
+  end
+
 end
