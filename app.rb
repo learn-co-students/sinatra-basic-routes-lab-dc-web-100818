fdef0084788a4a9b3aff4dac1415081b00c10f7c
Solee Lee <carolinesoleelee@gmail.com>
2018-10-29 10:51:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World"
+  end
+  get '/name' do
+    "My name is Caroline"
+  end
+
+  get '/hometown' do
+    "My hometown is gaithersburg"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Pizel Galaxy"
+  end
 end
