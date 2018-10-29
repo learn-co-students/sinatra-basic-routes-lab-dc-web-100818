356d03bad4a344aff8399d27b2b01dc728c3a9d4
jennyjean <jennyjean8675309@yahoo.com>
2018-10-29 10:13:21 -0400

    Created a variety of route methods using Sinatra

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get('/name') do
+    "My name is Jenny"
+  end
+
+  get('/hometown') do
+    "My hometown is Jerome, Idaho"
+  end
+
+  get('/favorite-song') do
+    "My favorite song is Mysterious Ways by U2"
+  end
+
 end
